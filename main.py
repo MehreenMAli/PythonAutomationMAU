55280c506ca3429dc1e9bc42399561d89ee5931f
Mehreen.Ali <meali@corcentric.com>
2022-08-25 12:46:21 +0500

    calcultor assigmnet

---
+++ main.py
@@ -0,0 +1,29 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+list =[1, 2, 3]
+list[2] = 4
+list.append(5)
+list.remove(2)
+print(list)
+# varlist=len(list)
+# for x in list:
+#     print(x)
+
+
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ task1.py
@@ -0,0 +1,113 @@
+# # list =[1, 2, 3]
+# # #Implement list
+# # list1 =['M', 'A', 'B']
+# # list2 =[1, 8, 3]
+# # Listm =list1+list2
+# # print(Listm)
+# #
+# # list.append(5)
+# # list.remove(1)
+# # print(list)
+# # varlist=len(list)
+# # for x in list:
+# #     print(x)
+# #
+# #
+# # #Implement DataType
+# # SS="******Implement DataType****"
+# # print(SS)
+# # x = "Mehreen" #String
+# # print(x,type(x)) #type is in builtant fn
+# # x=10 #integer
+# # print(x)
+# # y=5.2 #float
+# # print(y)
+# # c = 1j+1 #complex
+# # print(c,type(c))
+# # #String type single double or triple
+# # s='single a'
+# # print(s,type(s))
+# # d="double a senior test engineer"
+# # print(d)
+t='''mehreen
+     senior test engineer
+     10pearls
+'''
+print(t,type(t))
+# #
+# # #list
+# # ml="***Implement list***"
+# # print(ml)
+# # listm=[1,5,6]
+# # listm[2] = 4 #override value
+# # print(listm)
+# # Tuples="***Implement Tuples***"
+# # t=(1, 2, "mehreen")
+# # print(t,type(t))
+# # t=(20)
+# # print(t,type(t))
+#
+# #Range="***Implement RANGE***"
+# x=range(5)
+# print(x)
+#
+# #dic
+# dicc="***Implement dictonary***"
+# print(dicc)
+# d={
+#   'name': 'MA',
+#     "course":'python'
+# }
+# print(d['name'])
+# print(d,type(d))
+# #set
+# ss="*****set implementation****"# set work on order form and also collection of item,and unique value
+# myset={1,3,2,2}
+# print(myset)
+#
+#
+# fs="frozen set implementation"
+# print(fs)
+# F=frozenset()
+# print(F,type(F))
+# S = frozenset({"apple", "banana", "cherry"})
+# T=frozenset((1,3,2))
+# L=frozenset([1,2, "geo"])
+# print(S)
+# print(T)
+# print(L,type(L))
+# print(type(S))
+#
+# B="******boolen**"
+# print(B)
+# x = False
+# print(x)
+#
+# By="****Impement byte***"
+# print(By)
+# L=[10,20,36]
+# L[2] = 40
+# B=bytes(L)
+# print(B)
+# print(B[2])
+#
+#
+# By="****Impement byteARRAY***"
+# print(By)
+# L=[10,20,36]
+# L[2] = 40
+# B=bytearray(L)
+# print(B)
+# print(B[2])
+#
+# bb="****memoryview**"
+# x = memoryview(B)
+# print(x)
+# print(type(x))
+#
+#
+# nn="***none implementaion"
+# x = None
+# print(x,type(x))
+#
+#
